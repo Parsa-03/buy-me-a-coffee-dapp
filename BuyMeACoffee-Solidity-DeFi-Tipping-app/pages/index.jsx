f87828b291162ef516552928a6bd47c03f07454b
MrRomof <mfth821@gmail.com>
2024-07-09 17:24:39 +0330

    add ui

--- BuyMeACoffee-Solidity-DeFi-Tipping-app/pages/index.jsx
+++ BuyMeACoffee-Solidity-DeFi-Tipping-app/pages/index.jsx
@@ -1,13 +1,13 @@
-import abi from '../utils/BuyMeACoffee.json';
+import abi from "../utils/BuyMeACoffee.json";
 import { ethers } from "ethers";
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   // Contract Address & ABI
-  const contractAddress = "0xBf815Cf7a1237530A8274A4839Ea9A5d6630EA2d"
+  const contractAddress = "0xBf815Cf7a1237530A8274A4839Ea9A5d6630EA2d";
   const contractABI = abi.abi;
 
   // Component state
@@ -18,18 +18,18 @@ export default function Home() {
 
   const onNameChange = (event) => {
     setName(event.target.value);
-  }
+  };
 
   const onMessageChange = (event) => {
     setMessage(event.target.value);
-  }
+  };
 
   // Wallet connection logic
   const isWalletConnected = async () => {
     try {
       const { ethereum } = window;
 
-      const accounts = await ethereum.request({ method: 'eth_accounts' })
+      const accounts = await ethereum.request({ method: "eth_accounts" });
       console.log("accounts: ", accounts);
 
       if (accounts.length > 0) {
@@ -41,7 +41,7 @@ export default function Home() {
     } catch (error) {
       console.log("error: ", error);
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
@@ -49,8 +49,7 @@ export default function Home() {
 
       if (!ethereum) {
         console.log("Please install MetaMask");
-      } 
-      else {
+      } else {
         // Check if the wallet is connected
         if (!ethereum.isConnected()) {
           console.log("Please connect your wallet");
@@ -108,7 +107,6 @@ export default function Home() {
     }
   };
 
-
   const buyCoffee = async () => {
     try {
       const { ethereum } = window;
@@ -122,7 +120,7 @@ export default function Home() {
           signer
         );
 
-        console.log("buying coffee..")
+        console.log("buying coffee..");
         const coffeeTxn = await buyMeACoffee.buyCoffee(
           name ? name : "anon",
           message ? message : "Enjoy your coffee!",
@@ -164,7 +162,6 @@ export default function Home() {
       } else {
         console.log("Metamask is not connected");
       }
-
     } catch (error) {
       console.log(error);
     }
@@ -185,8 +182,8 @@ export default function Home() {
           address: from,
           timestamp: new Date(timestamp * 1000),
           message,
-          name
-        }
+          name,
+        },
       ]);
     };
 
@@ -196,11 +193,7 @@ export default function Home() {
     if (ethereum) {
       const provider = new ethers.providers.Web3Provider(ethereum, "any");
       const signer = provider.getSigner();
-      buyMeACoffee = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      );
+      buyMeACoffee = new ethers.Contract(contractAddress, contractABI, signer);
 
       buyMeACoffee.on("NewMemo", onNewMemo);
     }
@@ -209,7 +202,7 @@ export default function Home() {
       if (buyMeACoffee) {
         buyMeACoffee.off("NewMemo", onNewMemo);
       }
-    }
+    };
   }, []);
 
   return (
@@ -219,79 +212,91 @@ export default function Home() {
         <meta name="description" content="Tipping site" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.content}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Buy APA a Coffee!</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Buy APA a Coffee!
-        </h1>
+          {currentAccount ? (
+            <div>
+              <form>
+                <div className={styles.div_input}>
+                  <label className={styles.label}>Name</label>
 
-        {currentAccount ? (
-          <div>
-            <form>
-              <div>
-                <label>
-                  Name
-                </label>
-                <br />
+                  <input
+                    id="name"
+                    autoFocus
+                    className={styles.input}
+                    type="text"
+                    placeholder="Enter your name"
+                    onChange={onNameChange}
+                  />
+                </div>
 
-                <input
-                  id="name"
-                  type="text"
-                  placeholder="anon"
-                  onChange={onNameChange}
-                />
+                <div className={styles.div_input}>
+                  <label className={styles.label}>Send APA a message</label>
+
+                  <textarea
+                    rows={4}
+                    cols={50}
+                    className={`${styles.input} ${styles.textarea}`}
+                    placeholder="Enjoy your coffee!"
+                    id="message"
+                    onChange={onMessageChange}
+                    required
+                  ></textarea>
+                </div>
+                <div className={styles.div_btn}>
+                  <button
+                    className={`${styles.btn} ${styles.btn_send}`}
+                    type="button"
+                    onClick={buyCoffee}
+                  >
+                    Send 1 Coffee for 0.001ETH
+                  </button>
+                </div>
+              </form>
+            </div>
+          ) : (
+            <div className={styles.div_btn}>
+              <button onClick={connectWallet} className={styles.btn}>
+                {" "}
+                Connect your wallet{" "}
+              </button>
+            </div>
+          )}
+        </main>
+
+        {currentAccount && <h1 className={styles.memos}>Memos received</h1>}
+
+        {currentAccount &&
+          memos.map((memo, idx) => {
+            return (
+              <div
+                key={idx}
+                style={{
+                  border: "2px solid",
+                  borderRadius: "5px",
+                  padding: "5px",
+                  margin: "5px",
+                }}
+              >
+                <p style={{ fontWeight: "bold" }}>{memo.message}</p>
+                <p>
+                  From: {memo.name} at {memo.timestamp.toString()}
+                </p>
               </div>
-              <br />
-              <div>
-                <label>
-                  Send APA a message
-                </label>
-                <br />
-
-                <textarea
-                  rows={3}
-                  placeholder="Enjoy your coffee!"
-                  id="message"
-                  onChange={onMessageChange}
-                  required
-                >
-                </textarea>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  onClick={buyCoffee}
-                >
-                  Send 1 Coffee for 0.001ETH
-                </button>
-              </div>
-            </form>
-          </div>
-        ) : (
-          <button onClick={connectWallet}> Connect your wallet </button>
-        )}
-      </main>
-
-      {currentAccount && (<h1>Memos received</h1>)}
-
-      {currentAccount && (memos.map((memo, idx) => {
-        return (
-          <div key={idx} style={{ border: "2px solid", "borderRadius": "5px", padding: "5px", margin: "5px" }}>
-            <p style={{ "fontWeight": "bold" }}>{memo.message}</p>
-            <p>From: {memo.name} at {memo.timestamp.toString()}</p>
-          </div>
-        )
-      }))}
-
+            );
+          })}
+      </div>
       <footer className={styles.footer}>
         <a
           href="https://alchemy.com/?a=roadtoweb3weektwo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Created by @thatguyintech for Alchemy&apos;s Road to Web3 lesson two!        </a>
+          Created by @thatguyintech for Alchemy&apos;s Road to Web3 lesson two!{" "}
+        </a>
       </footer>
     </div>
-  )
-
+  );
 }
